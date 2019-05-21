9a494c989bd574e1f28d8fdfeb09a07481feef2f
Peter <peterchampion1997@gmail.com>
2019-05-21 02:49:45 +0100

    Initial Commit

---
+++ 2D Game Project (2019)/Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private Slider healthSlider;
+    private Slider staminaSlider;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(this);
+        }
+    }
+    void Start()
+    {
+
+        healthSlider = GameObject.Find("HealthSlider").GetComponent<Slider>();
+        staminaSlider = GameObject.Find("StaminaSlider").GetComponent<Slider>();
+    }
+    void Update()
+    {
+        
+    }
+
+    public void UpdateHealthUI(int healthValue)
+    {
+        healthSlider.value = healthValue;
+    }
+
+    public void UpdateStaminaUI(int staminaValue)
+    {
+        staminaSlider.value = staminaValue;
+    }
+}
---
+++ 2D Game Project (2019)/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private int maximumHealth = 100;
+    [SerializeField] private int currentHealth = 100;
+    [SerializeField] private int maximumStamina = 100;
+    [SerializeField] private int currentStamina = 100;
+
+    private Rigidbody2D RB;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpStrength; 
+
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
+        float xDirection = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        transform.position = new Vector2(transform.position.x + xDirection, transform.position.y);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RB.AddForce(new Vector2(0, jumpStrength), ForceMode2D.Impulse);
+        }
+    }
+
+    public void TakeDamage(int damageValue)
+    {
+        currentHealth -= damageValue;
+        GameManager.instance.UpdateHealthUI(currentHealth);
+    }
+
+    private void CheckHealth()
+    {
+        if (currentHealth <= 0)
+        {
+            // Needs functionality, respawn? Death screen?
+            Debug.Log("Player has died");
+        }
+    }
+
+    private void Attack()
+    {
+
+    }
+}
